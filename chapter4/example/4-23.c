d3dc1a9e35902cc644bf69dc5ced66ac4713bf75
wy471x <1004963485@qq.com>
2020-02-17 23:53:39 +0800

    chapter4 study finished.

---
+++ chapter4/example/4-23.c
@@ -0,0 +1,12 @@
+#include "../../include/apue.h"
+
+int main(void)
+{
+    if(chdir("/tmp") < 0)
+    {
+        err_sys("chdir failed");
+    }
+    printf("chdir to /tmp succeeded.\n");
+    return 0;
+}
+
