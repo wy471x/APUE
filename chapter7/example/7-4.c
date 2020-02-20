4643092a162a16fc0d8f8daa6ebf795880813c96
wy471x <1004963485@qq.com>
2020-02-21 01:32:55 +0800

    chapter7 study.

---
+++ chapter7/example/7-4.c
@@ -0,0 +1,11 @@
+#include "../../include/apue.h"
+
+int main(int argc, char *argv[])
+{
+    int i;
+    //for(i = 0; i < argc; i++)
+    for(i = 0; argv[i] != NULL; i++)    
+        printf("argv[%d]: %s\n", i, argv[i]);
+    return 0;
+}
+
