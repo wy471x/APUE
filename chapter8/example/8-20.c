fdf3131e2a400c1854e59e94e99248f92b5e37ab
wy471x <1004963485@qq.com>
2020-02-24 01:26:45 +0800

    chapter8 studying...

---
+++ chapter8/example/8-20.c
@@ -0,0 +1,20 @@
+#include "../../include/apue.h"
+#include <sys/wait.h>
+
+int main(void)
+{
+    pid_t pid;
+
+    if((pid = fork()) < 0){
+        err_sys("fork error");
+    }else if(pid == 0){
+        if(execl("/home/dunk/share/APUE/chapter8/example/testinterp",
+                 "testinterp", "myarg1", "MY ARG2", (char *)0) < 0)
+            err_sys("execl error");
+    }
+
+    if(waitpid(pid, NULL, 0) < 0)
+        err_sys("waitpid error");
+    return 0;
+}
+
